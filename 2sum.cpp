255be9c387f9095dbd7b51d0f951dc9b37e8f19e
Shivam Tiwari <102797639+ShivamTiwari27@users.noreply.github.com>
2023-06-12 00:36:24 +0530

    Update 2sum.cpp

--- 2sum.cpp
+++ 2sum.cpp
@@ -26,3 +26,40 @@ public:
     }
     
 };
+
+
+
+# Solution using hashmap in O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int , int > hash;
+        vector<int> answer;
+        int f=0;
+        for(int i=0;i<nums.size() ;  i++)
+        {
+            hash[nums[i]] = i;
+        }
+
+        for(int i=0;i<nums.size() ;  i++)
+        {
+            int k = target-nums[i];
+            if(hash.find(k)!=hash.end())
+            {
+                auto it = hash.find(k);
+                if(it->second!=i)
+                {
+                    answer.push_back(i);
+                    answer.push_back(it->second);
+                    f=1;
+                }
+            }
+            if(f)
+                break;
+            
+        }
+        return answer;
+    }
+    
+};
